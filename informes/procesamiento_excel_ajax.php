36a62d0c243c5885b3eb3fdb7796653afb28a2ff
rasief <rasief@gmail.com>
2021-02-02 13:56:58 -0500

    Generación de archivo zip consolidado de archivos de Excel

--- informes/procesamiento_excel_ajax.php
+++ informes/procesamiento_excel_ajax.php
@@ -119,13 +119,6 @@ function obtener_entes_territoriales($libro_excel, $col_dep, $col_mun, $mapa_dis
         //Se crea la hoja de destino
         $hoja_aux = $libro_excel->getSheetByName($nombre_hoja_aux);
         
-        //Se obtiene el número de columnas
-        $i = 1;
-        while ($hoja_aux->getCellByColumnAndRow($i, 1)->getValue() != null) {
-            $i++;
-        }
-        $num_cols = $i - 1;
-        
         //Se obtiene el número de filas
         $i = 1;
         while ($hoja_aux->getCellByColumnAndRow(1, $i)->getValue() != null) {
@@ -133,37 +126,42 @@ function obtener_entes_territoriales($libro_excel, $col_dep, $col_mun, $mapa_dis
         }
         $num_filas = $i - 1;
         
-        if ($num_cols >= 23) {
-            for ($i = 2; $i <= $num_filas; $i++) {
-                $nombre_celda_dep = $col_dep . (string)($i);
-                $nombre_celda_mun = $col_mun . (string)($i);
-                
-                $nom_dep = $hoja_aux->getCell($nombre_celda_dep)->getValue();
-                $nom_mun = $hoja_aux->getCell($nombre_celda_mun)->getValue();
-                $nom_dep_aux = $utilidades->simplificar_texto($nom_dep);
-                $nom_num_aux = $utilidades->simplificar_texto($nom_mun);
-                
-                //Se verifica si se trata de un distrito
-                $bol_distrito = false;
-                if (isset($mapa_distritos[$nom_dep_aux])) {
-                    $arr_aux = $mapa_distritos[$nom_dep_aux];
-                    if (in_array($nom_num_aux, $arr_aux)) {
-                        $bol_distrito = true;
-                    }
+        //Se obtiene el número de columnas
+        $i = 1;
+        while ($hoja_aux->getCellByColumnAndRow($i, 1)->getValue() != null) {
+            $i++;
+        }
+        $num_cols = $i - 1;
+        
+        for ($i = 2; $i <= $num_filas; $i++) {
+            $nombre_celda_dep = $col_dep . (string)($i);
+            $nombre_celda_mun = $col_mun . (string)($i);
+            
+            $nom_dep = $hoja_aux->getCell($nombre_celda_dep)->getValue();
+            $nom_mun = $hoja_aux->getCell($nombre_celda_mun)->getValue();
+            $nom_dep_aux = $utilidades->simplificar_texto($nom_dep);
+            $nom_num_aux = $utilidades->simplificar_texto($nom_mun);
+            
+            //Se verifica si se trata de un distrito
+            $bol_distrito = false;
+            if (isset($mapa_distritos[$nom_dep_aux])) {
+                $arr_aux = $mapa_distritos[$nom_dep_aux];
+                if (in_array($nom_num_aux, $arr_aux)) {
+                    $bol_distrito = true;
                 }
-                
-                if ($bol_distrito) {
-                    if (!isset($lista_entes[$nom_mun])) {
-                        $lista_entes[$nom_mun]["tipo"] = "M";
-                        $lista_entes[$nom_mun]["nom_dep"] = $nom_dep;
-                        $lista_entes[$nom_mun]["nom_mun"] = $nom_mun;
-                    }
-                } else {
-                    if (!isset($lista_entes[$nom_dep])) {
-                        $lista_entes[$nom_dep]["tipo"] = "D";
-                        $lista_entes[$nom_dep]["nom_dep"] = $nom_dep;
-                        $lista_entes[$nom_dep]["nom_mun"] = "";
-                    }
+            }
+            
+            if ($bol_distrito) {
+                if (!isset($lista_entes[$nom_mun])) {
+                    $lista_entes[$nom_mun]["tipo"] = "M";
+                    $lista_entes[$nom_mun]["nom_dep"] = $nom_dep;
+                    $lista_entes[$nom_mun]["nom_mun"] = $nom_mun;
+                }
+            } else {
+                if (!isset($lista_entes[$nom_dep])) {
+                    $lista_entes[$nom_dep]["tipo"] = "D";
+                    $lista_entes[$nom_dep]["nom_dep"] = $nom_dep;
+                    $lista_entes[$nom_dep]["nom_mun"] = "";
                 }
             }
         }
@@ -196,16 +194,12 @@ function validar_incluir_territorio($nom_dep, $nom_mun, $territorio, $lista_terr
     return $bol_incluir;
 }
 
-function procesar_monitoreo_aleatorio($nombre_arch, $nombre_tmp, $mapa_distritos, $fecha) {
-    $col_dep = "V";
-    $col_mun = "W";
-    
-    //Se crea la ruta que contendrá los archivos a generar
-    $arr_aux = explode("-", $fecha);
-    $ruta_base = "arch_excel/" . $arr_aux[0] . "/" . $arr_aux[1] . "/" . $arr_aux[2] . "/";
-    
+function procesar_archivo_excel($nombre_arch, $nombre_tmp, $mapa_distritos, $ruta_base, $col_dep, $col_mun) {
     //Se obtiene el listado de entes territoriales presentes
-    $lista_territorios = obtener_entes_territoriales(IOFactory::load($nombre_tmp), $col_dep, $col_mun, $mapa_distritos);
+    //echo("#" . $nombre_arch . "#" . $nombre_tmp . "#" . $col_dep . "#" . $col_mun . "#<br>");
+    $libro_des = IOFactory::load($nombre_tmp);
+    $lista_territorios = obtener_entes_territoriales($libro_des, $col_dep, $col_mun, $mapa_distritos);
+    $libro_des = null;
     
     foreach ($lista_territorios as $territorio_aux) {
         $libro_des = IOFactory::load($nombre_tmp);
@@ -215,7 +209,8 @@ function procesar_monitoreo_aleatorio($nombre_arch, $nombre_tmp, $mapa_distritos
         foreach ($lista_hojas as $nombre_hoja_aux) {
             //Se renombra la hoja
             $hoja_aux = $libro_des->getSheetByName($nombre_hoja_aux);
-            $hoja_aux->setTitle("ant-" . $nombre_hoja_aux);
+            $nombre_hoja_ant = substr("ant-" . $nombre_hoja_aux, 0, 30);
+            $hoja_aux->setTitle($nombre_hoja_ant);
             
             //Se crea la hoja de destino
             $hoja_des_aux = new Worksheet($libro_des, $nombre_hoja_aux);
@@ -258,11 +253,12 @@ function procesar_monitoreo_aleatorio($nombre_arch, $nombre_tmp, $mapa_distritos
             
             //Se borra la hoja base
             $indice_aux = $libro_des->getIndex(
-                $libro_des->getSheetByName("ant-" . $nombre_hoja_aux)
+                $libro_des->getSheetByName($nombre_hoja_ant)
             );
             $libro_des->removeSheetByIndex($indice_aux);
             
-            $cont_hojas++;
+            $hoja_aux = null;
+            $hoja_des_aux = null;
         }
         
         //Se crea la carpeta del territorio
@@ -271,80 +267,221 @@ function procesar_monitoreo_aleatorio($nombre_arch, $nombre_tmp, $mapa_distritos
         } else {
             $nombre_territorio = str_replace(",", "", str_replace(".", "", $territorio_aux["nom_mun"]));
         }
+        
+        //Se ajustan los nombres de algunos territorios para mantenerlos en la misma carpeta
+        switch ($nombre_territorio) {
+            case "BOGOTÁ D C":
+                $nombre_territorio = "BOGOTÁ DC";
+                break;
+            case "CARTAGENA":
+                $nombre_territorio = "CARTAGENA DE INDIAS";
+                break;
+            case "MOMPÓS":
+                $nombre_territorio = "SANTA CRUZ DE MOMPOX";
+                break;
+            case "QUINDIO":
+                $nombre_territorio = "QUINDÍO";
+                break;
+            case "VALLE":
+                $nombre_territorio = "VALLE DEL CAUCA";
+                break;
+        }
+        
         $ruta_territorio = $ruta_base . $nombre_territorio . "/";
         mkdir($ruta_territorio, 0777, true);
         
         //Se crea el archivo del territorio
         $writer = new XlsxWrite($libro_des);
         $writer->save($ruta_territorio . $nombre_territorio . " " . $nombre_arch);
+        $libro_des = null;
     }
-    //Se crea el archivo zip
-    /*$zip = new ZipArchive();
-    
-    $nombre_arch_zip = $ruta_base . "Salida.zip";
-    
-    if (file_exists($nombre_arch_zip)) {
-        unlink($nombre_arch_zip); 
+}
+
+function limpiar_directorio($directorio, $bol_borrar) {
+    $it = new RecursiveDirectoryIterator($directorio, RecursiveDirectoryIterator::SKIP_DOTS);
+    $archivos = new RecursiveIteratorIterator($it,
+                 RecursiveIteratorIterator::CHILD_FIRST);
+    foreach($archivos as $archivo_aux) {
+        if ($archivo_aux->isDir()){
+            rmdir($archivo_aux->getRealPath());
+        } else {
+            unlink($archivo_aux->getRealPath());
+        }
     }
     
-    if ($zip->open($nombre_arch_zip, ZIPARCHIVE::CREATE) != TRUE) {
-        die ("No se pudo crear el archivo");
+    if ($bol_borrar) {
+        rmdir($directorio);
+    }
+}
+
+function crear_paquete_zip($ruta_base, $contrasena) {
+    $arr_carpetas = scandir($ruta_base);
+    
+    foreach ($arr_carpetas as $carpeta_aux) {
+        if ($carpeta_aux != "." && $carpeta_aux != ".." && is_dir($ruta_base . $carpeta_aux)) {
+            //Se crea un archivo zip con el contenido de la carpeta
+            $zip = new ZipArchive();
+            
+            $nombre_arch_zip = $ruta_base . $carpeta_aux . ".zip";
+            
+            if (file_exists($nombre_arch_zip)) {
+                unlink($nombre_arch_zip); 
+            }
+            
+            if ($zip->open($nombre_arch_zip, ZIPARCHIVE::CREATE) != TRUE) {
+                die ("No se pudo crear el archivo");
+            }
+            
+            //Se agregan los archivos que se encuentran dentro de la carpeta
+            $arr_archivos = scandir($ruta_base . $carpeta_aux);
+            foreach ($arr_archivos as $archivo_aux) {
+                if ($archivo_aux != "." && $archivo_aux != ".." && is_file($ruta_base . $carpeta_aux . "/" . $archivo_aux)) {
+                    $zip->addFromString($archivo_aux, file_get_contents($ruta_base . $carpeta_aux . "/" . $archivo_aux));
+                    $zip->setEncryptionName($archivo_aux, ZipArchive::EM_AES_256, $contrasena);
+                }
+            }
+            $zip->close();
+            
+            //Se borra la carpeta de archivos
+            limpiar_directorio($ruta_base . $carpeta_aux, true);
+        }
     }
     
-    $zip->addFromString($nombre_arch, file_get_contents($ruta_base . $nombre_arch));
-    $zip->setEncryptionName($nombre_arch, ZipArchive::EM_AES_256, '1234');
+    //Se crea un único archivo zip con todo el contenido generado
+    $arr_partes = explode("/", $ruta_base);
     
-    $zip->close();*/
+    if (count($arr_partes) >= 4) {
+        $nombre_paquete = $arr_partes[0] . "/" . "archivos_" . $arr_partes[1] . "-" . $arr_partes[2] . "-" . $arr_partes[3] . ".zip";
+        
+        $zip = new ZipArchive();
+        
+        if (file_exists($nombre_paquete)) {
+            unlink($nombre_paquete); 
+        }
+        
+        if ($zip->open($nombre_paquete, ZIPARCHIVE::CREATE) != TRUE) {
+            die ("No se pudo crear el archivo consolidado");
+        }
+        
+        //Se agregan los archivos que se encuentran dentro de la carpeta
+        $arr_archivos = scandir($ruta_base);
+        foreach ($arr_archivos as $archivo_aux) {
+            if ($archivo_aux != "." && $archivo_aux != ".." && is_file($ruta_base . $archivo_aux)) {
+                $zip->addFromString($archivo_aux, file_get_contents($ruta_base . $archivo_aux));
+            }
+        }
+        $zip->close();
+        
+        //Se borra la carpeta de archivos
+        limpiar_directorio($arr_partes[0] . "/" . $arr_partes[1], true);
+    } else {
+        $nombre_paquete = "";
+    }
+    
+    return $nombre_paquete;
 }
 
 $opcion = $_POST['opcion'];
 
 switch ($opcion) {
     case "1": //Procesamiento de archivos de Excel
-        @$fecha = $utilidades->str_decode($_POST["fecha"]);
-        
-        //Se cargan los nombres de los archivos
-        $arr_nombres_aux = $_FILES["fil_arch"]["name"];
-        $arr_tmp_nombres_aux = $_FILES["fil_arch"]["tmp_name"];
-        
-        if (!is_array($arr_nombres_aux)) {
-            $arr_nombres_aux = array($arr_nombres_aux);
-            $arr_tmp_nombres_aux = array($arr_tmp_nombres_aux);
-        }
-        
-        //Lista de entes territoriales especiales (distritos)
-        $lista_distritos = $dbMunicipios->getListaDistritosClasificacion();
-        $mapa_distritos = array();
-        foreach ($lista_distritos as $distrito_aux) {
-            $nom_dep_aux = $utilidades->simplificar_texto($distrito_aux["nom_dep"]);
-            $nom_mun_aux = $utilidades->simplificar_texto($distrito_aux["nom_mun"]);
-            if (isset($mapa_distritos[$nom_dep_aux])) {
-                $arr_aux = $mapa_distritos[$nom_dep_aux];
-            } else {
-                $arr_aux = array();
-            }
-            if (!in_array($nom_mun_aux, $arr_aux)) {
-                array_push($arr_aux, $nom_mun_aux);
-            }
-            $mapa_distritos[$nom_dep_aux] = $arr_aux;
-        }
-        
-        foreach ($arr_nombres_aux as $k => $nombre_ori_aux) {
-            $extension_aux = strtolower($utilidades->get_extension_arch($nombre_ori_aux));
+        ?>
+        <div id="d_carga_interna">
+            <?php
+            $fecha = $utilidades->str_decode($_POST["fecha"]);
             
-            if ($extension_aux === "xlsx") {
-                //Se obtiene el tipo de archivo cargado
-                $tipo_arch = 0;
-                if (strpos(strtolower($nombre_ori_aux), "monitoreo aleatorio") === 0) {
-                    $tipo_arch = 1;
+            //Se crea la ruta que contendrá los archivos a generar
+            $arr_aux = explode("-", $fecha);
+            $ruta_base = "arch_excel/" . $arr_aux[0] . "/" . $arr_aux[1] . "/" . $arr_aux[2] . "/";
+            mkdir($ruta_base, 0777, true);
+            limpiar_directorio($ruta_base, false);
+            
+            //Se cargan los nombres de los archivos
+            $arr_nombres_aux = $_FILES["fil_arch"]["name"];
+            $arr_tmp_nombres_aux = $_FILES["fil_arch"]["tmp_name"];
+            
+            if (!is_array($arr_nombres_aux)) {
+                $arr_nombres_aux = array($arr_nombres_aux);
+                $arr_tmp_nombres_aux = array($arr_tmp_nombres_aux);
+            }
+            
+            //Lista de entes territoriales especiales (distritos)
+            $lista_distritos = $dbMunicipios->getListaDistritosClasificacion();
+            $mapa_distritos = array();
+            foreach ($lista_distritos as $distrito_aux) {
+                $nom_dep_aux = $utilidades->simplificar_texto($distrito_aux["nom_dep"]);
+                $nom_mun_aux = $utilidades->simplificar_texto($distrito_aux["nom_mun"]);
+                if (isset($mapa_distritos[$nom_dep_aux])) {
+                    $arr_aux = $mapa_distritos[$nom_dep_aux];
+                } else {
+                    $arr_aux = array();
                 }
+                if (!in_array($nom_mun_aux, $arr_aux)) {
+                    array_push($arr_aux, $nom_mun_aux);
+                }
+                $mapa_distritos[$nom_dep_aux] = $arr_aux;
+            }
+            
+            foreach ($arr_nombres_aux as $k => $nombre_ori_aux) {
+                $extension_aux = strtolower($utilidades->get_extension_arch($nombre_ori_aux));
                 
-                switch ($tipo_arch) {
-                    case 1: //Monitoreo aleatorio
-                        procesar_monitoreo_aleatorio($nombre_ori_aux, $arr_tmp_nombres_aux[$k], $mapa_distritos, $fecha);
-                        break;
+                if ($extension_aux === "xlsx") {
+                    //Se obtiene el tipo de archivo cargado
+                    $tipo_arch = 0;
+                    if (strpos(strtolower($nombre_ori_aux), "monitoreo aleatorio") === 0) {
+                        $tipo_arch = 1;
+                        $col_dep = "V";
+                        $col_mun = "W";
+                    } else if (strpos(strtolower($nombre_ori_aux), "monitoreo estado de salud") === 0) {
+                        $tipo_arch = 2;
+                        $col_dep = "S";
+                        $col_mun = "T";
+                    } else if (strpos(strtolower($nombre_ori_aux), "monitoreo viajeros") === 0) {
+                        $tipo_arch = 3;
+                        $col_dep = "P";
+                        $col_mun = "Q";
+                    } else if (strpos(strtolower($nombre_ori_aux), "necesidades alojamiento") === 0) {
+                        $tipo_arch = 4;
+                        $col_dep = "I";
+                        $col_mun = "H";
+                    } else if (strpos(strtolower($nombre_ori_aux), "necesidades transferencia") === 0) {
+                        $tipo_arch = 5;
+                        $col_dep = "I";
+                        $col_mun = "H";
+                    } else if (strpos(strtolower($nombre_ori_aux), "no asegurados") === 0) {
+                        $tipo_arch = 6;
+                        $col_dep = "I";
+                        $col_mun = "H";
+                    } else if (strpos(strtolower($nombre_ori_aux), "no contactados") === 0) {
+                        $tipo_arch = 7;
+                        $col_dep = "J";
+                        $col_mun = "I";
+                    } else if (strpos(strtolower($nombre_ori_aux), "viajeros no contactados") === 0) {
+                        $tipo_arch = 8;
+                        $col_dep = "O";
+                        $col_mun = "P";
+                    } else if (strpos(strtolower($nombre_ori_aux), "viajeros") === 0) {
+                        $tipo_arch = 9;
+                        $col_dep = "P";
+                        $col_mun = "Q";
+                    }
+                    
+                    if ($tipo_arch > 0) {
+                        procesar_archivo_excel($nombre_ori_aux, $arr_tmp_nombres_aux[$k], $mapa_distritos, $ruta_base, $col_dep, $col_mun);
+                    }
                 }
             }
+            ?>
+        </div>
+        <?php
+        $nombre_paquete = crear_paquete_zip($ruta_base, "1234");
+        
+        if ($nombre_paquete != "") {
+            ?>
+            <script id="ajax">
+                window.open("<?= $nombre_paquete ?>", "_blank");
+            </script>
+            <?php
         }
         break;
 }
